62e5a5e91bcb1e420fb78143d485e913ba66c1c9
Devan Beitel <devan.beitel@gmail.com>
2014-02-01 20:23:07 -0600

    Initial Commit

---
+++ Ribbit/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Ribbit
+//
+//  Created by Devan Beitel on 2/1/14.
+//  Copyright (c) 2014 Devan Beitel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Ribbit/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Ribbit
+//
+//  Created by Devan Beitel on 2/1/14.
+//  Copyright (c) 2014 Devan Beitel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
